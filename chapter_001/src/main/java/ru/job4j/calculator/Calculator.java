bffcfa5c6675c018a1305b5acc47938115f426a9
GrWo1 <s9197937187@gmail.com>
2020-03-16 14:02:19 +0300

    JavaDoc(#000023)

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculator;
+
+/**
+ * Calculator
+ *@author Sergey Afanasenko s9197937187@gmail.com
+ */
+
+public class Calculator {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int onePlusTwo = one + two;
+        System.out.println(onePlusTwo);
+        int sixDivTwo = six / two;
+        System.out.println(sixDivTwo);
+        int fiveMinusTwo = five - two;
+        System.out.println(fiveMinusTwo);
+        int fourTimeTwo = four * two;
+        System.out.println(fourTimeTwo);
+
+
+    }
+}
