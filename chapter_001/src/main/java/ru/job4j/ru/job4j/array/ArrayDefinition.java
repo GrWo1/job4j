869f088eefbebf441bff462cc716a665f2d5b983
GrWo1 <s9197937187@gmail.com>
2020-03-16 15:21:09 +0300

    Заполнение массива(#000026)

--- chapter_001/src/main/java/ru/job4j/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev I";
+        names[1] = "Petr Arsentev II";
+        names[2] = "Petr Arsentev III";
+        names[3] = "Petr Arsentev IV";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
