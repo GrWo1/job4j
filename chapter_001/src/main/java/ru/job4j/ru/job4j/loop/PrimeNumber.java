f1d66850d0e1b00bff6ad997060bbca8bda36a6e
GrWo1 <s9197937187@gmail.com>
2020-03-17 12:59:33 +0300

    Простые числа (доработка)(#000031)

--- chapter_001/src/main/java/ru/job4j/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/ru/job4j/loop/PrimeNumber.java
@@ -5,7 +5,7 @@ public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
         for (int number = 2; number <= finish; number++) {
-            if (CheckPrimeNumber.check(number) == true) {
+            if (CheckPrimeNumber.check(number)) {
                 count += 1;
             }
         }
