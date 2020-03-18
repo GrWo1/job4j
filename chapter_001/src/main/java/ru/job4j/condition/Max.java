d68da493434852847cc2614ea3265a5ca7685b92
GrWo1 <s9197937187@gmail.com>
2020-03-18 15:02:23 +0300

    Максимальное из двух (2)(#000034)

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left;
-        boolean condition = result >= right;
+        boolean condition = left >= right;
         return condition ? left : right;
     }
 }
